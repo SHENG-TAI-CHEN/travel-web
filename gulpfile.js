d49da5c2927abb9b10dcdbbde302352770cedfa9
SHENG-TAI-CHEN <ezfbk5806949@gmail.com>
2018-02-22 10:47:56 +0800

    herobox and context box

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,18 @@
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    autoprefixer = require('autoprefixer'),
-    postcss = require('gulp-postcss'),
-    cssvars = require('postcss-simple-vars'),
-    nested = require('postcss-nested'),
-    cssImport = require('postcss-import');
+require('./gulp/task/styles');
+require('./gulp/task/watch');
 
-gulp.task('default', function (){
-    console.log('You rae running default task');
-});
 
-gulp.task('html', function (){
-    console.log('watching html');
-});
 
-gulp.task('styles', function (){
-    return gulp.src('app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-        .pipe(gulp.dest('app/temp/styles'));
-});
 
-gulp.task('watch', function (){
-    watch('app/index.html', function (){
-        gulp.start('html');
-    });
 
-    watch('app/assets/styles/**/*.css', function (){
-        gulp.start('styles');
-    });
-});
+
+
+
+
+
+
+
+
+
+
 
